a5edfa251fb4dc67e7492c3ce265429a8c133757
alchonokk <galunia_suglob@mail.ru>
2021-11-06 15:47:13 +0300

    feat:add script for writing text from console

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path'); 
+const outfile = fs.createWriteStream(path.join('02-write-file','yourtext.txt'),'utf-8', console.log('Please, write your message here'));
+const { stdin, stdout } = require('process');
+const readline = require('readline');
+const rl = readline.createInterface({input:stdin, output:stdout });
+
+
+rl.on('line', (line) => {
+    if (line=== 'exit'){
+        process.exit();
+    } else {
+        outfile.write(`${line}\n`);
+    }   
+})
+
+process.on('exit', () => {
+      console.log('Goodbye!');
+});
+
